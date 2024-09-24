3665708b229f336e1db2f9f17f08ddca800309b1
xirhxq <xirhxq@gmail.com>
2024-09-24 12:32:41 +0800

    change rate to 20hz; frame_id as 'map'

--- scripts/vicon2mavros.py
+++ scripts/vicon2mavros.py
@@ -10,25 +10,27 @@ class Vicon2Mavros:
         for i in range(1, 11):
             rospy.Subscriber(f"/vicon/p230_{i}/p230_{i}", TransformStamped, partial(self.callback, i=i))
         self.publishers = [rospy.Publisher(f"/uav{i}/mavros/vision_pose/pose", PoseStamped, queue_size=10) for i in range(1, 11)]
+        self.pose = [PoseStamped() for i in range(1, 11)]
+
+        rospy.Timer(rospy.Duration(0.05), self.timer_callback)
         rospy.spin()
 
-
     def callback(self, msg: TransformStamped, i):
-        pose = PoseStamped()
-
-        pose.header = msg.header
+        self.pose[i - 1].header = msg.header
+        self.pose[i - 1].header.frame_id = 'map'
         
-        pose.pose.position.x = msg.transform.translation.x
-        pose.pose.position.y = msg.transform.translation.y
-        pose.pose.position.z = msg.transform.translation.z
+        self.pose[i - 1].pose.position.x = msg.transform.translation.x
+        self.pose[i - 1].pose.position.y = msg.transform.translation.y
+        self.pose[i - 1].pose.position.z = msg.transform.translation.z
 
-        pose.pose.orientation.x = msg.transform.rotation.x
-        pose.pose.orientation.z = msg.transform.rotation.z
-        pose.pose.orientation.y = msg.transform.rotation.y
-        pose.pose.orientation.w = msg.transform.rotation.w
+        self.pose[i - 1].pose.orientation.x = msg.transform.rotation.x
+        self.pose[i - 1].pose.orientation.z = msg.transform.rotation.z
+        self.pose[i - 1].pose.orientation.y = msg.transform.rotation.y
+        self.pose[i - 1].pose.orientation.w = msg.transform.rotation.w
 
-        self.publishers[i-1].publish(pose)
-
+    def timer_callback(self, event):
+        for i in range(1, 11):
+            self.publishers[i - 1].publish(self.pose[i - 1])
 
 if __name__ == "__main__":
     Vicon2Mavros()
